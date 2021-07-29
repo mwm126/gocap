d47bdc50643db2f5f9711332f9cab569d806b09c
Mark Meredith <mark.meredith@netl.doe.gov>
2021-07-28 21:28:22 -0400

    cleanup

--- cap/knocker.go
+++ cap/knocker.go
@@ -31,82 +31,8 @@ type PortKnocker struct {
 	entropy [32]byte
 }
 
-func (sk *PortKnocker) makePacket(uname, pword string, timestamp int32) []byte {
-	OTP := getOTP(sk.yubikey, sk.entropy[:])
-
-	var initVec [16]byte
-	digest := makeSHADigest(sk.entropy[:], OTP[:])
-	copy(initVec[:], digest[:16])
-	challenge, response := getChallengeResponse(sk.yubikey, OTP, sk.entropy[:])
-	serial := sk.yubikey.findSerial()
-
-	var user [32]byte
-	var auth [16]byte
-	var ssh [16]byte
-	var server [16]byte
-	copy(user[:], []byte(uname))
-	var buf bytes.Buffer
-	copy(auth[12:], net.ParseIP("74.109.234.77").To4())
-	copy(ssh[12:], net.ParseIP("74.109.234.77").To4())
-	copy(server[12:], net.ParseIP("104.154.139.11").To4())
-
-	buf.Write(auth[:])
-	buf.Write(ssh[:])
-	buf.Write(server[:])
-	buf.Write(OTP[:])
-	buf.Write(user[:])
-	buf.Write(sk.entropy[:])
-	plainBlock := plainBlockWithChecksum(buf.Bytes())
-
-	aeskey := makeSHADigest(response[:], challenge[:])
-
-	key := []byte(aeskey[:])
-	plaintext := plainBlock
-	if len(plaintext)%aes.BlockSize != 0 {
-		panic("plaintext is not a multiple of the block size")
-	}
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err)
-	}
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
-	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
-	iv := []byte(initVec[:])
-	mode := cipher.NewCBCEncrypter(block, iv)
-	mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
-
-	var trimmedCiphertext [160]byte
-	copy(trimmedCiphertext[:], ciphertext[16:])
-
-	buf = bytes.Buffer{}
-	binary.Write(&buf, binary.LittleEndian, timestamp)
-	binary.Write(&buf, binary.LittleEndian, serial)
-	binary.Write(&buf, binary.LittleEndian, initVec)
-	binary.Write(&buf, binary.LittleEndian, challenge)
-	binary.Write(&buf, binary.LittleEndian, trimmedCiphertext)
-
-	header, _ := hex.DecodeString("823220d0df9234263797c5d0c5fee27ab087f86e76f82efe0bb386cc65ae879f")
-	macBlock := buf.Bytes()
-	footer, _ := hex.DecodeString("50266198ce6bae2069546cbcae0f80ba847598f674f5d7343f90e6c6e56dfa8a")
-	digest = makeSHADigest(header, macBlock, footer)
-
-	buf = bytes.Buffer{}
-	err = binary.Write(&buf, binary.LittleEndian, macBlock)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = binary.Write(&buf, binary.LittleEndian, digest)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return buf.Bytes()
-}
-
 func (sk *PortKnocker) Knock(uname, pword string) {
-
 	time.Sleep(1 * time.Second)
-
 	response, err := ntp.Query("pool.ntp.org")
 	timestamp := int32(time.Now().Add(response.ClockOffset).Unix())
 
@@ -125,6 +51,71 @@ func (sk *PortKnocker) Knock(uname, pword string) {
 	conn.Write(packet)
 	conn.Write(timestampBytes)
 	conn.Close()
+}
+
+func (sk *PortKnocker) makePacket(uname, pword string, timestamp int32) []byte {
+	OTP := getOTP(sk.yubikey, sk.entropy[:])
+
+	var initVec [16]byte
+	digest := makeSHADigest(sk.entropy[:], OTP[:])
+	copy(initVec[:], digest[:16])
+	challenge, response := getChallengeResponse(sk.yubikey, OTP, sk.entropy[:])
+	aeskey := makeSHADigest(response[:], challenge[:])
+
+	var user [32]byte
+	var auth [16]byte
+	var ssh [16]byte
+	var server [16]byte
+	copy(user[:], []byte(uname))
+	copy(auth[12:], net.ParseIP("74.109.234.77").To4())
+	copy(ssh[12:], net.ParseIP("74.109.234.77").To4())
+	copy(server[12:], net.ParseIP("104.154.139.11").To4())
+
+	var buf bytes.Buffer
+	buf.Write(auth[:])
+	buf.Write(ssh[:])
+	buf.Write(server[:])
+	buf.Write(OTP[:])
+	buf.Write(user[:])
+	buf.Write(sk.entropy[:])
+	plainBlock := plainBlockWithChecksum(buf.Bytes())
+
+	key := []byte(aeskey[:])
+	if len(plainBlock)%aes.BlockSize != 0 {
+		panic("plaintext is not a multiple of the block size")
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plainBlock))
+	iv := []byte(initVec[:])
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], plainBlock)
+
+	var trimmedCiphertext [160]byte
+	copy(trimmedCiphertext[:], ciphertext[16:])
+
+	serial := sk.yubikey.findSerial()
+
+	buf = bytes.Buffer{}
+	binary.Write(&buf, binary.LittleEndian, timestamp)
+	binary.Write(&buf, binary.LittleEndian, serial)
+	binary.Write(&buf, binary.LittleEndian, initVec)
+	binary.Write(&buf, binary.LittleEndian, challenge)
+	binary.Write(&buf, binary.LittleEndian, trimmedCiphertext)
+
+	header, _ := hex.DecodeString("823220d0df9234263797c5d0c5fee27ab087f86e76f82efe0bb386cc65ae879f")
+	macBlock := buf.Bytes()
+	footer, _ := hex.DecodeString("50266198ce6bae2069546cbcae0f80ba847598f674f5d7343f90e6c6e56dfa8a")
+	digest = makeSHADigest(header, macBlock, footer)
+
+	buf = bytes.Buffer{}
+	binary.Write(&buf, binary.LittleEndian, macBlock)
+	binary.Write(&buf, binary.LittleEndian, digest)
+	return buf.Bytes()
 }
 
 func getOTP(yk Yubikey, entropy []byte) OneTimePassword {
