626317bd78443f2640fb5047e4f3de31484e0f59
Mark Meredith <mark.meredith@netl.doe.gov>
2021-07-30 09:54:26 -0400

    Fix YAML parsing

--- cap/networks_test.go
+++ cap/networks_test.go
@@ -8,5 +8,13 @@ import (
 func TestCapConnection(t *testing.T) {
 	networks := getNetworks()
 
-	assert.Equal(t, Network{}, networks["external"])
+	assert.Equal(t, Network{
+		true,
+		(""),
+		("204.154.140.51"),
+		("204.154.139.11"),
+		62201,
+		("172.16.0.1"),
+		("204.154.140.10"),
+	}, networks["external"])
 }
