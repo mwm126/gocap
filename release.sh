718c53f2b80dc209f2869be503d733abbb524fed
Mark Meredith <mark.meredith@netl.doe.gov>
2021-09-23 21:09:42 -0400

    Disable Windows

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ NOTES="Build with fyne-cross"
 
 ASSET_MAC=fyne-cross/Gocap.${TAG}_Mac.zip
 ASSET_LINUX=fyne-cross/gocap.${TAG}_Linux.tar.gz
-ASSET_WINDOWS=fyne-cross/gocap.${TAG}_Windows.zip
+# ASSET_WINDOWS=fyne-cross/gocap.${TAG}_Windows.zip
 
 docker build .fyne-cross/linux/ -t capclient-linux
 docker build .fyne-cross/windows/ -t capclient-windows
@@ -15,16 +15,16 @@ docker build .fyne-cross/windows/ -t capclient-windows
 git clean -fdx
 go generate ./...
 fyne-cross linux -image capclient-linux:latest
-fyne-cross windows -image capclient-windows:latest
+# fyne-cross windows -image capclient-windows:latest  ## TODO: Use separate shell script
 fyne-cross darwin --app-id "com.aeolustec.capclient"
 
 zip -r -j ${ASSET_MAC} fyne-cross/dist/darwin-amd64/gocap.app
 ln -f fyne-cross/dist/linux-amd64/gocap.tar.gz ${ASSET_LINUX}
-ln -f fyne-cross/dist/windows-amd64/gocap.exe.zip ${ASSET_WINDOWS}
+# ln -f fyne-cross/dist/windows-amd64/gocap.exe.zip ${ASSET_WINDOWS}
 
 gh release create ${TAG} \
     --draft \
+    -n "${NOTES}" -t "${TITLE}" \
     ${ASSET_MAC} \
     ${ASSET_LINUX} \
-    ${ASSET_WINDOWS} \
-    -n "${NOTES}" -t "${TITLE}"
+    # ${ASSET_WINDOWS} ## TODO:  Use separate shell script
