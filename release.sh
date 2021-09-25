001fbefe1c182fc55075824bf5a788ec59ea01d5
Mark Meredith <mark.meredith@netl.doe.gov>
2021-09-24 21:33:55 -0400

    Fix windows fyne-cross build

--- release.sh
+++ release.sh
@@ -4,27 +4,39 @@ set -euo pipefail
 TAG=v21.9.23
 TITLE="CAP Client Release ${TAG}"
 NOTES="Link in libyubikey and libykpers"
+ASSETS=""
 
-ASSET_MAC=fyne-cross/Gocap.${TAG}_Mac.zip
-ASSET_LINUX=fyne-cross/gocap.${TAG}_Linux.tar.gz
-# ASSET_WINDOWS=fyne-cross/gocap.${TAG}_Windows.zip
+function build_linux {
+    docker build .fyne-cross/linux/ -t capclient-linux
+    fyne-cross linux -image capclient-linux:latest
+    ASSET="fyne-cross/gocap.${TAG}_Linux.tar.gz"
+    ln -f fyne-cross/dist/linux-amd64/gocap.tar.gz ${ASSET}
+    ASSETS="${ASSETS} ${ASSET}"
+}
 
-docker build .fyne-cross/linux/ -t capclient-linux
-docker build .fyne-cross/windows/ -t capclient-windows
+function build_mac {
+    ASSET="fyne-cross/Gocap.${TAG}_Mac.zip"
+    ASSETS="${ASSETS} ${ASSET}"
+    # fyne-cross darwin --app-id "com.aeolustec.capclient"
+    zip -r -j ${ASSET} fyne-cross/dist/darwin-amd64/gocap.app
+}
+
+function build_windows {
+    docker build .fyne-cross/windows/ -t capclient-windows
+    fyne-cross windows -image capclient-windows:latest
+    ASSET="fyne-cross/gocap.${TAG}_Windows.zip"
+    ln -f fyne-cross/dist/windows-amd64/gocap.exe.zip ${ASSET}
+    ASSETS="${ASSETS} ${ASSET}"
+}
 
 git clean -fdx
 go generate ./...
-fyne-cross linux -image capclient-linux:latest
-# fyne-cross windows -image capclient-windows:latest  ## TODO: Use separate shell script
-fyne-cross darwin --app-id "com.aeolustec.capclient"
 
-zip -r -j ${ASSET_MAC} fyne-cross/dist/darwin-amd64/gocap.app
-ln -f fyne-cross/dist/linux-amd64/gocap.tar.gz ${ASSET_LINUX}
-# ln -f fyne-cross/dist/windows-amd64/gocap.exe.zip ${ASSET_WINDOWS}
+build_linux
+# build_mac
+build_windows
 
 gh release create ${TAG} \
     --draft \
     -n "${NOTES}" -t "${TITLE}" \
-    ${ASSET_MAC} \
-    ${ASSET_LINUX} \
-    # ${ASSET_WINDOWS} ## TODO:  Use separate shell script
+    "${ASSETS}"
