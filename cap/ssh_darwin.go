52403d0256406e4e130901369f0663b20f3162f9
Mark Meredith <mark.meredith@netl.doe.gov>
2021-12-10 11:43:02 -0500

    Fix darwin

--- cap/ssh_darwin.go
+++ cap/ssh_darwin.go
@@ -1,12 +1,13 @@
 package cap
 
 import (
+	"aeolustec.com/capclient/cap/connection"
 	_ "embed"
 	"log"
 )
 
 //go:generate curl --insecure "https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe" --output embeds/putty.exe
 
-func run_ssh(conn Connection) {
+func run_ssh(conn connection.Connection) {
 	log.Println("TODO")
 }
