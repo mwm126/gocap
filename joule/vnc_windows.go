d9401af30e851b5c4b66850fc2adf52cfad9b064
Mark Meredith <mark.meredith@netl.doe.gov>
2021-12-28 19:21:33 -0500

    build libjpeg-turbo

--- joule/vnc_windows.go
+++ joule/vnc_windows.go
@@ -1,24 +1,68 @@
 package joule
 
 import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
-	"strconv"
-
-	"aeolustec.com/capclient/cap"
 )
 
-func RunVnc(conn *cap.Connection, otp, displayNumber string) {
-	cmd := exec.Command(
-		"x-terminal-emulator",
-		"--",
-		"vnc",
-		"localhost",
-		"-p",
-		strconv.Itoa(VNC_LOCAL_PORT),
+//go:embed turbovnc/bin/*
+//go:embed turbovnc/share/*
+var content embed.FS
+
+func VncCmd(vncviewer_path, otp string, localPort int) string {
+	return fmt.Sprintf(
+		"env -u LD_LIBRARY_PATH %s 127.0.0.1::%d -Password='%s'",
+		vncviewer_path,
+		localPort,
+		otp,
 	)
-	err := cmd.Run()
+}
+
+func RunVnc(otp, displayNumber string, localPort int) {
+	vnchome, err := ioutil.TempDir("", "capclient")
 	if err != nil {
-		log.Println("gnome-terminal FAIL: ", err)
+		log.Fatal("could not open tempfile", err)
+	}
+	defer os.RemoveAll(vnchome)
+
+	fs.WalkDir(content, ".", func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			dirname := vnchome + "/" + path
+			os.Mkdir(dirname, 0755)
+			return nil
+		}
+		src := path
+		input, err := content.ReadFile(src)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		dest := vnchome + "/" + path
+		err = ioutil.WriteFile(dest, input, 0644)
+		if err != nil {
+			fmt.Println("Error creating", dest)
+			fmt.Println(err)
+			return err
+		}
+		return nil
+	})
+
+	vnc_cmd := vnchome + "/turbovnc/bin/vncviewer"
+	err = os.Chmod(vnc_cmd, 0755)
+	if err != nil {
+		log.Fatal("could not make ", vnc_cmd, " executable because ", err)
+	}
+
+	cmd_string := VncCmd(vnc_cmd, otp, localPort)
+	cmd := exec.Command("sh", "-c", cmd_string)
+	log.Println("\n\n\nRunVnc: ", cmd)
+	if output, err := cmd.CombinedOutput(); err != nil {
+		log.Println("vncviewer output: ", string(output))
+		log.Println("vncviewer error: ", err)
 	}
 }
