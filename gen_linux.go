1839a1a046aec3280d6c3a4e18e0c0e44a03c222
Mark Meredith <mark.meredith@netl.doe.gov>
2021-12-29 15:00:25 -0500

    fix

--- gen_linux.go
+++ gen_linux.go
@@ -1,10 +1,5 @@
 package main
 
-// For fyne-cross on Linux to build the Windows distribution
-//go:generate curl -L --insecure "https://sourceforge.net/projects/turbovnc/files/2.2.7/TurboVNC-2.2.7-x64.exe/download" --output TurboVNC-2.2.7-x64.exe
-//go:generate innoextract -e TurboVNC-2.2.7-x64.exe --output-dir joule/TurboVNC-2.2.7
-//go:generate curl --insecure "https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe" --output ssh/embeds/putty.exe
-
 // For fyne-cross on Linux to build the Linux distribution (or for development)
 //go:generate cmake -S subprojects/turbovnc/ -B build-turbo -DCMAKE_INSTALL_PREFIX=joule/turbovnc/ -GNinja -DTVNC_BUILDSERVER=0
 //go:generate cmake --build build-turbo --target install
