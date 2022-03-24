5c2a4bc170eeac02e5ef5a1144ff9bd577691338
Mark Meredith <mwm126@pm.me>
2022-03-24 11:30:50 -0400

    Use sshpass (if available) (#36)

--- ssh/ssh_linux.go
+++ ssh/ssh_linux.go
@@ -3,38 +3,59 @@ package ssh
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 
 	"aeolustec.com/capclient/cap"
 )
 
-func find_term() string {
-	path, err := exec.LookPath("x-terminal-emulator")
+func find_term() []string {
+	path, err := exec.LookPath("gnome-terminal")
 	if err == nil {
-		return path
+		return []string{path, "--"}
 	}
-	log.Println("x-terminal-emulator not found; assuming gnome-terminal")
+	log.Println("gnome-terminal not found; trying xterm")
 
-	path, err = exec.LookPath("gnome-terminal")
+	path, err = exec.LookPath("xterm")
 	if err == nil {
-		return path
+		return []string{path, "-e"}
 	}
-	log.Println("Warning: gnome-terminal not found.  Falling back to xterm")
+	log.Println("xterm not found.")
 
-	return "xterm"
+	return []string{}
 }
 
 func run_ssh(conn *cap.Connection) {
-	terminal := find_term()
-	cmd := exec.Command(
-		terminal,
-		"-e",
-		fmt.Sprintf("ssh %s@%s -p %d\n",
-			conn.GetUsername(),
-			"localhost",
-			cap.SSH_LOCAL_PORT),
+	args := find_term()
+	if len(args) == 0 {
+		log.Println("Could not start SSH session")
+		return
+	}
+	sshpass, err := exec.LookPath("sshpass")
+	if err != nil {
+		log.Println("Could not find sshpass; re-enter password")
+	} else {
+		args = append(args, sshpass, "-e")
+	}
+
+	args = append(args,
+		"ssh",
+		"-o",
+		"UserKnownHostsFile=/dev/null",
+		"-o",
+		"StrictHostKeyChecking=no",
+		"-l",
+		conn.GetUsername(),
+		"-p",
+		fmt.Sprint(cap.SSH_LOCAL_PORT),
+		"127.0.0.1",
 	)
-	err := cmd.Run()
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Env = append(os.Environ(),
+		fmt.Sprintf("SSHPASS=%s", conn.GetPassword()),
+	)
+
+	err = cmd.Run()
 	if err != nil {
 		log.Println("Error: could not start SSH session in terminal: ", err)
 	}
